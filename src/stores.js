e3c7420dc01293d9e512b2f69c1259208ea91cf8
Shalnark <65479699+Shounaks@users.noreply.github.com>
2023-10-15 18:50:55 +0530

    Initial commit

---
+++ src/stores.js
@@ -0,0 +1,13 @@
+// import { atom } from 'nanostores';
+// import { useStore } from "@nanostores/solid";
+
+// export const isOpen = atom(true);
+
+// export const navbarToggler = () => {
+//     isOpen.set(!useStore(isOpen));
+//     return useStore(isOpen) ? "active" : "";
+// }
+
+// export const navbarStatus = () => {
+//     return useStore(isOpen);
+// }
