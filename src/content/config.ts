5bb46fb9fdae8e36c04c28e1b4fef45f128e8828
Shalnark <65479699+Shounaks@users.noreply.github.com>
2023-10-21 10:32:25 +0530

    Adding Tag Support to Site

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,7 @@ const blog = defineCollection({
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 		readTime: z.string().optional(),
-		tags: z.array(z.string()).optional()
+		tags: z.array(z.string())
 	}),
 });
 
