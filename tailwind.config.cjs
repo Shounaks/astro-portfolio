3837be6bdbf30f3594209f287ef0a465929451df
Shalnark <65479699+Shounaks@users.noreply.github.com>
2023-11-28 17:23:31 +0530

    fixing config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./docs/**/*", './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./dist/**/*.{astro,html,js,jsx,ts,tsx}", './src/**/*.{astro,html,js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
 	},
