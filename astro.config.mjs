7fe4904a6b48e78e886410fb1fb091262a99320d
Shalnark <65479699+Shounaks@users.noreply.github.com>
2023-11-28 16:01:25 +0530

    testing new blog layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shounaks.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), solidJs()],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
