8fae72bca36acd9c3c82e0fa60b7d5b9b1fe3ffa
shounak <shounakbhalerao777@gmail.com>
2024-08-28 14:47:42 +0530

    changing site url for creation of correct OpenGraph Image URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shounaks.github.io',
+  site: 'https://shounakb.netlify.app/',
   integrations: [mdx(), sitemap(), tailwind()],
 });
